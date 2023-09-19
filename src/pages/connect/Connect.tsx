9e64fd361dc5e7400f0830685c6d191e6b409540
Nastya <skreka.nastya@gmail.com>
2023-09-19 22:10:34 +0300

    add form validate react-hook-form and yup

--- src/pages/connect/Connect.tsx
+++ src/pages/connect/Connect.tsx
@@ -1,3 +1,5 @@
+import ConnectForm from "./components/ConnectForm";
+
 const Connect = () => {
     return (
         <section className="pt-60 pb-20 max-container">
@@ -5,9 +7,65 @@ const Connect = () => {
                 <h1 className="max-md:text-center max-md:leading-normal max-xl:text-4xl text-8xl mb-5 font-semibold">Let's <span className="text-blue-600">Connect</span></h1>
                 <p className="leading-normal mb-12 text-center  mx-auto text-xl">Have a big idea? We would love to hear from you.</p>
             </div> 
-            
+            <div className="flex flex-row justify-between items-center lg:pt-12 pb-10">     
+            <div className="flex flex-row w-full md:w-8/12 xl:w-1/2 2xl:w-5/12 mx-auto">
+            <div className="flex flex-row justify-center items-center rounded-2xl mt-10 lg:mt-0 shadow-lg border border-transparent p-6 w-full">
+                <ConnectForm/>
+            </div>    
+            </div>
+            </div>        
         </section>
     );
 };
 
 export default Connect;
+
+/*
+  <form className="w-full max-w-md">
+                <div className="mb-6">
+                    <label htmlFor="name" className="block text-gray-700 text-xl mb-3" >
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Your name"
+                        className=" bg-gray-200 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-primary-3"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="email" className="block text-gray-700 text-xl mb-3 ">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Your email"
+                        className="w-full bg-gray-200 px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-primary-3"
+                    />
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="message" className="block text-gray-700 text-xl mb-3">
+                            Message
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            placeholder="Type your message here..."
+                            className="w-full bg-gray-200 px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-primary-3"
+                            
+                        />
+                    </div>
+                    <div className="text-center">
+                        <button
+                            type="submit"
+                            className="px-6 py-4 bg-primary-3 w-full text-white rounded-lg hover:bg-primary-4 focus:outline-none focus:ring focus:border-primary-3"
+                        >
+                            Submit
+                        </button>
+                    </div>
+            </form>
+
+*/
---
+++ src/pages/connect/components/ConnectForm.tsx
@@ -0,0 +1,110 @@
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"; 
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const schema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup.string().email("Invalid email").required("Email is required"),
+    message: yup.string().required("Message is required"),
+  });
+
+const ContactForm = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema), 
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-md w-full">
+    <div className="mb-10">
+        <div className="mb-2">
+                <label htmlFor="name" className="block text-gray-700 text-xl mb-3">
+                Name
+                </label>
+                <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                    <input
+                    {...field}
+                    type="text"
+                    placeholder="Your name"
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-primary-3 bg-gray-200"
+                    />
+                )}
+            />
+        </div>
+        {errors.name && (
+            <span className="block mb-6 error-message text-sm text-red-600">Name is required</span>
+        )}
+      </div>
+      <div className="mb-10">
+        <div className="mb-2">
+            <label htmlFor="email" className="block text-gray-700 text-xl mb-3 ">
+            Email
+            </label>
+            <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+                <input
+                {...field}
+                type="email"
+                placeholder="Your email"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-primary-3 bg-gray-200"
+                />
+            )}
+            />
+        </div>
+            {errors.email && (
+                <span className=" block error-message  mb-6 text-sm  text-red-600">Email is required</span>
+            )}   
+      </div>  
+      <div className="mb-10">
+        <div className="mb-2">
+            <label htmlFor="message" className="block text-gray-700 text-xl mb-3">
+                Message
+            </label>
+            <Controller
+            name="message"
+            control={control}
+            render={({ field }) => (
+                <textarea
+                {...field}
+                placeholder="Type your message here..."
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-primary-3 bg-gray-200"
+                />
+            )}
+            />
+        </div>
+            {errors.message && (
+                <span className="error-message block  mb-6 text-sm text-red-600">Message is required</span>
+            )}
+      </div>
+      <div className="text-center">
+        <button
+          type="submit"
+          className="px-6 py-4 bg-primary-3 w-full text-white rounded-lg hover:bg-primary-4 focus:outline-none focus:ring focus:border-primary-3"
+        >
+          Submit
+        </button>
+      </div>
+     
+    </form>
+  );
+};
+
+export default ContactForm;
